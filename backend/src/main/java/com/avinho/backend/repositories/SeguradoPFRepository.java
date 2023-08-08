c940d7b347fd3bd1477e99e04c25231a9bcf3a36
alvaro <alvaromarttinho@gmail.com>
2023-08-07 22:28:18 -0300

    Refatorando entidade Segurado

--- backend/src/main/java/com/avinho/backend/repositories/SeguradoPFRepository.java
+++
@@ -1,7 +0,0 @@
-package com.avinho.backend.repositories;
-
-import com.avinho.backend.entities.segurado.SeguradoPF;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeguradoPFRepository extends JpaRepository<SeguradoPF, Long> {
-}
--- backend/src/main/java/com/avinho/backend/repositories/SeguradoPJRepository.java
+++
@@ -1,7 +0,0 @@
-package com.avinho.backend.repositories;
-
-import com.avinho.backend.entities.segurado.SeguradoPJ;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeguradoPJRepository extends JpaRepository<SeguradoPJ, Long> {
-}
